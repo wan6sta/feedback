74d2f509297a0b11903ad48f4aaeab4b368dbdd4
wan6sta <wan6sta@gmail.com>
2023-01-14 06:12:28 +0300

    Refactor

--- app/router/router.ts
+++ app/router/router.ts
@@ -3,4 +3,8 @@ import { emailController } from '../controllers/EmailController'
 
 export const router = Router()
 
+router.post('/', (req, res) => {
+  res.json('Feedback server')
+})
+
 router.post('/message', emailController.sendOnEmail)
